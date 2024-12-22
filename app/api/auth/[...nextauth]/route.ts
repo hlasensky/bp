145c692b0ecb6547d08b8b883febf74ab09fb912
hlasensky <tomas.hlasensky@seznam.cz>
2024-12-22 13:40:44 +0100

    chore: :passport_control: adding provider for credentials

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,38 @@
 import prisma from "@/prisma/prisma";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
-	providers: [],
+	providers: [
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				username: { label: "Username", type: "text", placeholder: "jsmith" },
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials) {
+                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+                
+				if (!credentials) {
+					return null;
+				}
+
+				const user2 = await prisma.user.findFirst({
+					where: {
+						email: credentials.username,
+						password: credentials.password
+					}
+				})
+
+				if (user) {
+					return user;
+				} else {
+					return null;
+				}
+			},
+		}),
+	],
 	adapter: PrismaAdapter(prisma),
 });
 
